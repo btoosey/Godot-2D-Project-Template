cc19c9802752997c0526ea71f12d00d51611732b
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2025-08-01 12:13:50 +0100

    FIX: intilialised fsm on game start

--- scripts/components/state_machines/game_state_machine/game_states/game_play_state.gd
+++ scripts/components/state_machines/game_state_machine/game_states/game_play_state.gd
@@ -2,3 +2,7 @@ extends State
 
 # Use the following to move to this state:
 # game_state_machine._on_transition_requested(game_state_machine.current_state, State.StateID.PLAY)
+
+func enter():
+	print("Entering State: Play")
+	print("Introduce and rename states as needed")
